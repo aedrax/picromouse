4075784f9642c791504537301bd307a9763accc5
Samuel Roth <sejr@me.com>
2015-04-17 14:48:30 -0400

    More testing

--- src/mouse.py
+++ src/mouse.py
@@ -4,7 +4,7 @@ MICROMOUSE ALGORITHM
 Python Edition
 
 Written by Sam Roth
-Last revised: 16 APRIL 2015
+Last revised: 17 APRIL 2015
 
 """
 
@@ -68,8 +68,10 @@ class Mouse:
         self.s_distance = sensor_read(s)
         self.w_distance = sensor_read(w)
 
-# TESTS FOR MOUSE
-# For each method
+
+class TestMouseMethods(unittest.TestCase):
+    def return_zero(self):
+        return 0
 
 
 class Maze:
@@ -120,9 +122,16 @@ class Maze:
             self.map.append(row)
             x += 1
 
-# TESTS FOR MAZE
-# Tons of tests for init
-# Check resulting cell coordinates and weights
+
+class TestMazeMethods(unittest.TestCase):
+
+    # To add:
+    # Methods for initialized cells, specifically their coordinates
+    # and responding weights. Do references from maze match up with
+    # the object attributes x and y?g
+
+    def return_zero(self):
+        return 0
 
 
 def sensor_read(sensor):
